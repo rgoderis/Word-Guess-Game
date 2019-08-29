26cb8d8cf170321a56d84556355f68f8e09a5df2
Ryan Goderis <rgoderis789@gmail.com>
2019-08-29 17:21:15 -0400

    created logic for user guess to display on DOM in an updated array called hangmanWordText

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,52 +4,33 @@ var guessLeft = 12;
 var guessSoFar = [];
 var userGuess;
 var hangmanWord;
-var blankWord = []
+var blankWord = [];
+var dashedWord = ''
 
 // Star Wars Character
-var computerChoices = [{
-    name: "Chewbacca",
-    img: ""
-    },
-    {
-    name: "Darth Vader",
-    img: "",
-    },
-    {
-    name: "Luke Skywalker",
-    img: "",
-    },
-    {
-    name: "Han Solo",
-    img: "",
-    },        
-    {
-    name: "C3PO",
-    img: "",
-    },        
-    {
-    name: "R2D2",
-    img: "",
-    }     
-]
+// switch to an array
+var computerChoices = ["chewbacca",
+"darthvader",
+"lukeskywalker",
+"hansolo",
+"C3PO",
+"R2D2"]
 
 // create function to display the results to page DOM
 function updateDisplay(){
     document.querySelector("#wins").innerHTML = wins;
     document.querySelector("#guess-left").innerHTML = guessLeft;
     document.querySelector("#guess-so-far").innerHTML = guessSoFar;
-    document.querySelector("#hangman-word").innerHTML = hangmanWord;
-    document.querySelector("#blank-word").innerHTML = blankWord;
+    document.querySelector("#hangman-word").innerHTML = hangmanWordText;
 }
 
 // create function for computer to grab random word from array
 function computerRandomWord(){
     // create function that will display blank word that is length of hangman word
-    hangmanWord = computerChoices[Math.floor(Math.random() * computerChoices.length)].name.toLowerCase();
-    for(var i = 0; i < hangmanWord.length; i++){
-        blankWord.push("_");
-    }
+    hangmanWord = computerChoices[Math.floor(Math.random() * computerChoices.length)].toLowerCase();  
 }
+
+
 
 // create function to reset scores
 function reset(){
@@ -57,22 +38,41 @@ function reset(){
     guessSoFar = [];
 }
 
+// calling function for computer to grab a random word from the computer choices array
 computerRandomWord();
+
+// pushes _ for each letter in the hangmanWord
+for(var i = 0; i < hangmanWord.length; i++){
+    blankWord.push("_");
+    dashedWord = blankWord.join(" ")
+}
+
+// sets the dashed word the text that should display in the html as the hangman word
+hangmanWordText = dashedWord
+
+// updating the display after choosing a random word and displaying it as dashes
 updateDisplay();
+
 // create a onkeyup envent function to grab userGuess
 document.onkeyup = function(event){
     userGuess = event.key
-
 // cycle through hangmanWord
     for(var i = 0; i < hangmanWord.length; i++){
-        if(userGuess === hangmanWord[i]){
-            console.log("correct" + userGuess)
-            blankWord.push(userGuess)
-            blankWord.pop("_")
+        // check to see if the user guess is a character in the hangman word
+        if(userGuess === hangmanWord.charAt(i)){
+            // sets the user guess at the index it appears in 
+            blankWord[i] = userGuess
+            console.log(blankWord[i])
+            // updates dashedWord array with user guess
+            dashedWord = blankWord.join(" ")
+            // updates the hangman word to display as the dashed array text
+            hangmanWordText = dashedWord;
+            // updates DOM display
             updateDisplay()
-
+            console.log("you guessed right!!!")
+            // for whatever reason the dashed word array is not updating with the blank word array
         }
 
     }
-   
-}
+}   
+
