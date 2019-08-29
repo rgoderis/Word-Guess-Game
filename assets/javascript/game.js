cd7ecedcf551c7c0bd2f36d28040ac41316ca037
Ryan Goderis <rgoderis789@gmail.com>
2019-08-29 15:49:54 -0400

    created for loop to compare userGuess to hangmanWord

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ var guessLeft = 12;
 var guessSoFar = [];
 var userGuess;
 var hangmanWord;
+var blankWord = []
 
 // Star Wars Character
 var computerChoices = [{
@@ -38,11 +39,16 @@ function updateDisplay(){
     document.querySelector("#guess-left").innerHTML = guessLeft;
     document.querySelector("#guess-so-far").innerHTML = guessSoFar;
     document.querySelector("#hangman-word").innerHTML = hangmanWord;
+    document.querySelector("#blank-word").innerHTML = blankWord;
 }
 
 // create function for computer to grab random word from array
 function computerRandomWord(){
-    hangmanWord = computerChoices[Math.floor(Math.random() * computerChoices.length)].name;
+    // create function that will display blank word that is length of hangman word
+    hangmanWord = computerChoices[Math.floor(Math.random() * computerChoices.length)].name.toLowerCase();
+    for(var i = 0; i < hangmanWord.length; i++){
+        blankWord.push("_");
+    }
 }
 
 // create function to reset scores
@@ -56,5 +62,17 @@ updateDisplay();
 // create a onkeyup envent function to grab userGuess
 document.onkeyup = function(event){
     userGuess = event.key
+
+// cycle through hangmanWord
+    for(var i = 0; i < hangmanWord.length; i++){
+        if(userGuess === hangmanWord[i]){
+            console.log("correct" + userGuess)
+            blankWord.push(userGuess)
+            blankWord.pop("_")
+            updateDisplay()
+
+        }
+
+    }
    
 }
