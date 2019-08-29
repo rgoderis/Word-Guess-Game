5d1735edb378902a4d2267813237737a1aa269fe
Ryan Goderis <rgoderis789@gmail.com>
2019-08-28 22:46:17 -0400

    created function to grab word from computer choices array, created function to reset guesses left and guesses so far

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -36,17 +36,23 @@ var computerChoices = [{
 function updateDisplay(){
     document.querySelector("#wins").innerHTML = wins;
     document.querySelector("#guess-left").innerHTML = guessLeft;
-    document.querySelector("#user-guess").innerHTML = userGuess;
-    document.querySelector("#hangman-word").innerHTML = hangmanWprd;
+    document.querySelector("#guess-so-far").innerHTML = guessSoFar;
+    document.querySelector("#hangman-word").innerHTML = hangmanWord;
 }
 
 // create function for computer to grab random word from array
-
+function computerRandomWord(){
+    hangmanWord = computerChoices[Math.floor(Math.random() * computerChoices.length)].name;
+}
 
 // create function to reset scores
+function reset(){
+    guessLeft = 12;
+    guessSoFar = [];
+}
 
-
-
+computerRandomWord();
+updateDisplay();
 // create a onkeyup envent function to grab userGuess
 document.onkeyup = function(event){
     userGuess = event.key
